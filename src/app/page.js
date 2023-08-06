ff502c24aded0c0bbfd896e8dec97e1c50cb9fc4
vishnu_p <vishnu.p@richylife.ae>
2023-08-06 16:36:51 +0400

    about home page desktp done

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,18 @@
-import { HeroSection, ServiceBanner } from '@/components';
-import Card from '@/components/ui/card/card';
-import CardV1 from '@/components/ui/card/cardv1';
-import Image from 'next/image';
+import { HeroSection, ServiceBanner } from "@/components";
+import Aboutsection from "@/components/home/aboutsection";
+import Card from "@/components/ui/card/card";
+import CardV1 from "@/components/ui/card/cardv1";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <HeroSection
-        posterurl={'/assets/home/heroposter.png'}
-        type={'video'}
-        url={'/assets/home/hero.mp4'}
+        posterurl={"/assets/home/heroposter.png"}
+        type={"video"}
+        url={"/assets/home/hero.mp4"}
       />
+      <Aboutsection />
       <ServiceBanner />
       {/* <section>
         <Card />
---
+++ src/components/home/aboutsection.jsx
@@ -0,0 +1,70 @@
+import Image from "next/image";
+import React from "react";
+
+function Aboutsection() {
+  const data = [
+    {
+      title: "Private events ",
+      number: "11k",
+    },
+    {
+      title: "Customer Satisfaction",
+      number: "100%",
+    },
+    {
+      title: "Private events ",
+      number: "100%",
+    },
+    {
+      title: "Private events ",
+      number: "180+",
+    },
+    {
+      title: "Private events ",
+      number: "30",
+    },
+  ];
+  return (
+    <div className="py-20 bg-white">
+      <div className="lg:flex w-full max-w-[1200px] lg:h-[320px] mx-auto bg-white">
+        <div className="hidden lg:block relative w-full aspect-[557/557] lg:w-2/3 lg:max-w-[512px]">
+          <Image src="/assets/test/farrari.png" fill priority alt="car" />
+        </div>
+        <div className="bg-white text-black mx-20">
+          <h2 className="text-teal-900 text-[42px] font-extrabold leading-10">
+            RICHYLIFE <br />
+            CLUB{" "}
+          </h2>
+
+          <p className="py-10 text-md font-thin text-secondary">
+            Quis risus sed vulputate odio ut. Arcu vitae elementum curabitur
+            vitae nunc sed. Mauris a diam maecenas sed enim ut sem. Non enim
+            praesent elementum facilisis. Sapien nec sagittis aliquam malesuada
+            bibendum arcu vitae elementum
+          </p>
+          <p className=" text-md font-thin text-secondary">
+            Quis risus sed vulputate odio ut. Arcu vitae elementum curabitur
+            vitae nunc sed. Mauris a diam maecenas sed enim ut sem. Non enim
+            praesent elementum facilisis. Sapien nec sagittis aliquam malesuada
+            bibendum arcu vitae elementum
+          </p>
+        </div>
+      </div>
+
+      <div className="container mx-auto">
+        <div className="mt-10 flex flex-wrap gap-10 justify-center lg:justify-between max-w-[1000px] mx-auto">
+          {data.map((e, index) => (
+            <div key={index}>
+              <div className="text-emerald-600 text-[61.71px] font-black">
+                {e.number}
+              </div>
+              <p className="text-black text-xs">{e.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Aboutsection;
