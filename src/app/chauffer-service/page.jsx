b786b470f287c8a5cdbaffe5eefce1baf6fc3a93
vishnu_p <vishnu.p@richylife.ae>
2023-08-17 12:17:39 +0400

    card for chauffer :

--- src/app/chauffer-service/page.jsx
+++ src/app/chauffer-service/page.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import { HeroSection, SectionHeading } from "@/components";
+import CardSq from "@/components/ui/card/CardSq";
+import Image from "next/image";
+import React from "react";
 
 const page = () => {
+  const data = [
+    {
+      title: "Airport Transfer",
+      image: "/assets/home/heroposter.png",
+    },
+    {
+      title: "Half/Full Day",
+      image: "/assets/home/heroposter.png",
+    },
+    {
+      title: "Dubai City Tour",
+      image: "/assets/home/heroposter.png",
+    },
+    {
+      title: "A-B Transfer",
+      image: "/assets/home/heroposter.png",
+    },
+    {
+      title: "Events",
+      image: "/assets/home/heroposter.png",
+    },
+  ];
   return (
     <main>
       <h1 className="sr-only">
         Chauffer & Airport Transfer Services - Richylife Club
       </h1>
+      <HeroSection
+        posterurl={"/assets/home/heroposter.png"}
+        type={"video"}
+        alt="Luxury Car rentals"
+        url={"/assets/home/hero.mp4"}
+      />
+
+      <div className="mt-10 max-w-[1200px] mx-auto px-5">
+        <SectionHeading title={"Our Services"} description={""} />
+        <div className="container mx-auto mt-10">
+          <div className="grid grid-cols-2 md:grid-cols-5 lg:grid-cols-5 justify-items-center gap-8">
+            {data.map((e, index) => (
+              <CardSq key={index} title={e.title} />
+            ))}
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
---
+++ src/components/ui/card/CardSq.jsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import React from "react";
+
+function CardSq({ key, title }) {
+  return (
+    <div key={key} className="relative  w-full aspect-[357/406]">
+      <Image
+        src={"/assets/home/heroposter.png"}
+        alt={"sss"}
+        fill
+        style={{
+          objectFit: "cover",
+        }}
+      />
+      <div className="absolute p-4 text-white w-full h-fit bg-black bg-opacity-60 bottom-0 left-0 right-0 z-10">
+        <div>
+          <h2 className="uppercase font-medium tracking-wide text-sm font-poppins">
+            {title}
+          </h2>
+          {/* <span className="text-gray-400 text-sm">
+          Starting from AED212
+        </span> */}
+        </div>
+
+        <button className="mt-2 px-2 py-2 bg-primary text-gold">
+          <a
+            href="https://wa.me/1234567890"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Book Now
+          </a>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CardSq;
