cb46d4a32a3c4c4b4de674ca8fd0b5cc338bc885
Rahul B <rahul.b@richylife.ae>
2023-08-17 17:31:51 +0400

    Services Data fetching enabled

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'swiperjs.com',
-        port: '',
-      },
-    ],
+    domains: ['res.cloudinary.com'],
   },
 };
 
