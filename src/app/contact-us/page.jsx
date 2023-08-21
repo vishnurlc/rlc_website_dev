221bce2773eadfaa44fec75860eb8d26ea1a4495
vishnu_p <vishnu.p@richylife.ae>
2023-08-21 09:29:50 +0400

    contact page

--- src/app/contact-us/page.jsx
+++ src/app/contact-us/page.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import { ContactForm, HeroSection } from "@/components";
+import React from "react";
 
 const page = () => {
-  return <main>page</main>;
+  return (
+    <main>
+      <h1 className="sr-only">Contact Us - Richylife Club</h1>
+      <HeroSection
+        posterurl={"/assets/chauffer/hero.png"}
+        type={"video"}
+        alt="Luxury Car rentals"
+        url={"/assets/home/hero.mp4"}
+      />
+
+      <ContactForm title={"Contact Us"} />
+    </main>
+  );
 };
 
 export default page;
