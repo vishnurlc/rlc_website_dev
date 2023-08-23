5f51a2b37c4bde66dd7a5b9ec0ed34f1b0d31761
vishnu_p <vishnu.p@richylife.ae>
2023-08-23 16:58:12 +0400

    animated btn

--- src/components/premiumjetski/AnimatedBtn.jsx
+++ src/components/premiumjetski/AnimatedBtn.jsx
@@ -1,10 +1,16 @@
-'use client';
-import React from 'react';
+"use client";
+import React from "react";
 
-const AnimatedBtn = ({ styles, text }) => {
+const AnimatedBtn = ({ styles, text, msg }) => {
   return (
     <button className={`px-6 py-2 hover:scale-105 ${styles} transition-all`}>
-      {text}
+      <Link
+        href={`https://wa.me/${process.env.WHATSAPP_NUMBER}?text=${msg}?`}
+        target="_blank"
+        className="flex justify-center items-center gap-4"
+      >
+        {text}
+      </Link>
     </button>
   );
 };
