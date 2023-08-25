f2012773562434017b8584116ee1216c3163caf0
vishnu_p <vishnu.p@richylife.ae>
2023-08-25 15:39:16 +0400

    about page

--- src/app/aboutus/page.jsx
+++ src/app/aboutus/page.jsx
@@ -1,7 +1,113 @@
-import React from 'react';
+import { HeroSection } from "@/components";
+import Image from "next/image";
+import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <HeroSection
+        posterurl={"/assets/rentacar/banner.png"}
+        type={"video"}
+        alt="Luxury Car rentals"
+        url={"/assets/home/hero.mp4"}
+      />
+
+      <div className="max-w-[1200px] mx-auto flex flex-col gap-6 px-6">
+        <div className=" mx-auto mt-10">
+          <div className=" flex flex-col items-center justify-center text-justify">
+            <h3 className="text-2xl md:text-5xl text-gold capitalize font-inter">
+              About Us
+            </h3>
+            <p className="text-gray-500 my-6 ">
+              Welcome to Richylife Club, where the pursuit of luxury travel and
+              life's finest indulgences converges with a vibrant community of
+              like-minded individuals. Our mission is to redefine the way you
+              experience the world, curating extraordinary journeys that
+              encompass elegance, exclusivity, and meaningful connections.
+            </p>
+            <p className="text-gray-500 my-6 ">
+              At Richylife Club, we go beyond the ordinary, curating exceptional
+              experiences that epitomize the art of luxury travel. Our team of
+              experts scours the globe to handpick the most exquisite hotels,
+              resorts, dining establishments, and activities, ensuring that each
+              recommendation is a testament to unparalleled quality. We pride
+              ourselves on providing our members with exclusive access to hidden
+              gems, from secluded villas to behind-the-scenes encounters,
+              transforming your travels into remarkable stories of opulence and
+              adventure. But Richylife Club is more than just a showcase of
+              lavishness; it's a community where friendships flourish and
+              memories are created. Through curated group trips, themed events,
+              and an online platform designed for interaction, we foster
+              connections that transcend borders and cultures. Join us in
+              embracing a life where luxury knows no bounds and where every
+              journey becomes a celebration of the finer things in life.
+            </p>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="relative w-full h-[30vh]  md:h-[50vh] max-h-[700px]">
+            <Image
+              src="/assets/jetskipage/jetskisample.jpg"
+              fill
+              alt="Premium jet ski"
+              style={{
+                objectFit: "cover",
+                borderRadius: "8px",
+              }}
+            />
+          </div>
+          <div className=" flex flex-col items-start justify-center text-justify">
+            <h3 className="text-2xl md:text-5xl text-gold capitalize font-inter">
+              Our vision
+            </h3>
+            <p className="text-gray-500 my-6 ">
+              Our vision is to establish a thriving and exclusive community of
+              individuals united by their unwavering passion for luxury travel
+              and a shared appreciation for life's exquisite pleasures. Through
+              curated experiences, impeccable service, and a strong sense of
+              camaraderie, we aim to transform the way our members perceive and
+              engage with the world. We envision a haven where the art of travel
+              is elevated to new heights, allowing our members to indulge in
+              unparalleled hospitality, discover hidden gems, and forge profound
+              connections with both the world and one another.
+            </p>
+          </div>
+        </div>
+        {/* left */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className=" flex flex-col items-start justify-center text-justify">
+            <h3 className="text-2xl md:text-5xl text-gold capitalize font-inter">
+              Our Mission
+            </h3>
+            <p className="text-gray-500 my-6 ">
+              At Richylife Club, our mission is to curate and cultivate a
+              world-class community of individuals who are passionate about
+              luxury travel and the finer things in life. Through meticulous
+              curation, exclusive access, and heartfelt connections, we are
+              dedicated to redefining the art of travel and enriching lives. We
+              strive to be the ultimate catalyst for unforgettable experiences,
+              lifelong friendships, and personal growth, all while promoting
+              sustainability and positive global impact. Our mission is to
+              empower our members to embark on extraordinary journeys, both
+              outward and inward, that celebrate the essence of living life
+              luxuriously and purposefully.
+            </p>
+          </div>
+          <div className="relative w-full h-[30vh]  md:h-[50vh] max-h-[700px]">
+            <Image
+              src="/assets/jetskipage/jetskisample.jpg"
+              fill
+              alt="Premium jet ski"
+              style={{
+                objectFit: "cover",
+                borderRadius: "8px",
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
