f9e29d039f1d1788ae721b8a87aca331e3b979ec
Rahul B <rahul.b@richylife.ae>
2023-08-26 21:17:44 +0400

    Added the amenties

--- src/components/amenitys/Amenitys.jsx
+++ src/components/amenitys/Amenitys.jsx
@@ -1,5 +1,5 @@
-"use client";
-import React from "react";
+'use client';
+import React from 'react';
 import {
   FaWifi,
   FaFire,
@@ -12,28 +12,31 @@ import {
   FaMusic,
   FaConciergeBell,
   FaTemperatureHigh,
-} from "react-icons/fa";
+} from 'react-icons/fa';
 
 function Amenitys({ data }) {
   console.log(data.data);
 
   const amenitiesIcons = {
-    "wi-fi": <FaWifi />,
-    "air-conditioning": <FaSnowflake />,
-    "live-bbq": <FaFire />,
-    "friendly-crew": <FaPeopleCarry />,
-    "sun-bed": <FaBed />,
-    safety: <FaLifeRing />,
-    "fishing-gears": <FaFish />,
-    kitchen: <FaBlender />,
-    entertainment: <FaMusic />,
-    heating: <FaTemperatureHigh />,
-    "room-service": <FaConciergeBell />,
+    'wi-fi': <FaWifi key="wifi" />,
+    'air-conditioning': <FaSnowflake key="air" />,
+    'live-bbq': <FaFire key="fire" />,
+    'friendly-crew': <FaPeopleCarry key="people" />,
+    'sun-bed': <FaBed key="bed" />,
+    safety: <FaLifeRing key="ring" />,
+    'fishing-gears': <FaFish key="fish" />,
+    kitchen: <FaBlender key={'blender'} />,
+    entertainment: <FaMusic key="music" />,
+    heating: <FaTemperatureHigh key="temp" />,
+    'room-service': <FaConciergeBell key="con" />,
   };
   return (
     <div className="mt-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-5 justify-items-center">
       {data?.data.map((item, index) => (
-        <div className=" flex flex-col justify-center items-center gap-3">
+        <div
+          className=" flex flex-col justify-center items-center gap-3"
+          key={index}
+        >
           <span className="">{amenitiesIcons[item.attributes.slug]}</span>
           {item.attributes.name}
         </div>
