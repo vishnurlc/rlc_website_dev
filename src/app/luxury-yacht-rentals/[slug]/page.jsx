6c3bbdb26a1d6d76eb5738ed43e716603c2eb133
vishnu_p <vishnu.p@richylife.ae>
2023-08-30 18:47:51 +0400

    yatch genereteparams

--- src/app/luxury-yacht-rentals/[slug]/page.jsx
+++ src/app/luxury-yacht-rentals/[slug]/page.jsx
@@ -3,12 +3,12 @@ import {
   GalleryJet,
   HeroCarousel,
   RichTextComponent,
-} from '@/components';
-import Amenitys from '@/components/amenitys/Amenitys';
-import AnimatedBtn from '@/components/premiumjetski/AnimatedBtn';
+} from "@/components";
+import Amenitys from "@/components/amenitys/Amenitys";
+import AnimatedBtn from "@/components/premiumjetski/AnimatedBtn";
 
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 export async function generateMetadata({ params }) {
   try {
@@ -22,17 +22,17 @@ export async function generateMetadata({ params }) {
     ).then((res) => res.json());
     return {
       title:
-        yacht.data[0].attributes.name || 'Luxury Yachts for Rental In Dubai ',
+        yacht.data[0].attributes.name || "Luxury Yachts for Rental In Dubai ",
       description:
         yacht.data[0].attributes.description ||
-        'Luxury Yachts rental with Richylife Club',
+        "Luxury Yachts rental with Richylife Club",
       openGraph: {
-        type: 'website',
+        type: "website",
         title:
-          yacht.data[0].attributes.name || 'Luxury Yachts for Rental In Dubai ',
+          yacht.data[0].attributes.name || "Luxury Yachts for Rental In Dubai ",
         description:
           yacht.data[0].attributes.description ||
-          'Luxury Yachts rental with Richylife Club',
+          "Luxury Yachts rental with Richylife Club",
         images: [
           {
             url: `${yacht.data[0].attributes.image.data[0].attributes.url}`,
@@ -68,7 +68,7 @@ export async function getData(slug) {
 
     return data;
   } catch (error) {
-    console.log('s', error);
+    console.log("s", error);
     return {};
   }
 }
@@ -78,148 +78,152 @@ export default async function YacthDetail({ params: { slug } }) {
 
   return (
     <main>
-      <div>
-        <HeroCarousel data={yacht?.data[0].attributes.image} />
-        <div className="pt-4 px-6 flex items-center justify-start md:justify-end gap-5 max-w-[1200px] mx-auto">
-          <h2 className="text-right">
-            <span className="text-xl text-primary">
-              AED{yacht.data[0].attributes.price}/Day
-            </span>
-          </h2>
-
-          <AnimatedBtn
-            styles={'rounded-sm bg-primary text-white'}
-            text={'Book Now'}
-            msg={'Hi, I would like to know about your services.'}
-          />
-        </div>
-      </div>
-      <div className="max-w-[1200px] mx-auto py-10 md:py-16 px-6 flex flex-col gap-8 md:gap-16">
-        <div>
+      {yacht && (
+        <>
           <div>
-            <h1 className="text-sm md:text-base text-justify md:text-left text-gray-500">
-              {yacht.data[0].attributes.make.data.attributes.make}
-            </h1>
-            <span className="inline-block my-2 md:my-4 font-inter text-primary font-semibold text-2xl md:text-[40px]">
-              {yacht.data[0].attributes.name}
-            </span>
-          </div>
-
-          <RichTextComponent
-            bio={yacht.data[0].attributes.description}
-            style={
-              'text-sm md:text-base text-justify md:text-left text-gray-500'
-            }
-          />
-        </div>
-
-        <div>
-          <h2 className="inline-block mb-4 md:mb-8 font-inter text-primary font-semibold text-xl md:text-[40px]">
-            Destinations
-          </h2>
-
-          <RichTextComponent
-            bio={yacht.data[0].attributes.destinations}
-            style={
-              'text-sm md:text-base text-justify md:text-left text-gray-500'
-            }
-          />
-        </div>
-
-        <div>
-          <h2 className="inline-block mb-4 md:mb-8 font-inter text-primary font-semibold text-xl md:text-[40px]">
-            Technical Specifications
-          </h2>
-          <ul className="grid grid-cols-1 md:grid-cols-2 gap-x-9  ">
-            <li className="flex font-inter text-sm md:text-lg w-full items-center justify-between py-2 md:py-4  border-b border-[#E4EBF0] ">
-              <span>Make</span>
-              <span className="text-[#8a97a4]">
-                {yacht.data[0].attributes.make.data.attributes.make}
-              </span>
-            </li>
-            <li className="flex font-inter text-sm md:text-lg w-full items-center justify-between py-2 md:py-4  border-b border-[#E4EBF0] ">
-              <span>Model</span>
-              <span className="text-[#8a97a4]">
-                {yacht.data[0].attributes.model.data.attributes.model}
-              </span>
-            </li>
-            <li className="flex font-inter text-sm md:text-lg w-full items-center justify-between py-2 md:py-4  border-b border-[#E4EBF0] ">
-              <span>Length</span>
-              <span className="text-[#8a97a4]">
-                {yacht.data[0].attributes.technicalspec.length}
-              </span>
-            </li>
-            <li className="flex font-inter text-sm md:text-lg w-full items-center justify-between py-2 md:py-4  border-b border-[#E4EBF0] ">
-              <span>Capacity</span>
-              <span className="text-[#8a97a4]">
-                {`${yacht.data[0].attributes.technicalspec.capacity} People ${
-                  yacht.data[0].attributes.technicalspec.overnight
-                    ? `, ${yacht.data[0].attributes.technicalspec.overnight} overnight`
-                    : ''
-                }`}
-              </span>
-            </li>
-            <li className="flex font-inter text-sm md:text-lg w-full items-center justify-between py-2 md:py-4  border-b border-[#E4EBF0] ">
-              <span>Category</span>
-              <span className="text-[#8a97a4]">
-                {yacht.data[0].attributes.technicalspec.category}
-              </span>
-            </li>
-            <li className="flex font-inter text-sm md:text-lg w-full items-center justify-between py-2 md:py-4  border-b border-[#E4EBF0] ">
-              <span>Rooms</span>
-              <span className="text-[#8a97a4]">
-                {yacht.data[0].attributes.technicalspec.rooms}
-              </span>
-            </li>
-            {yacht.data[0].attributes.technicalspec.stereo_system && (
-              <li className="flex font-inter text-sm md:text-lg w-full items-center justify-between py-2 md:py-4  border-b border-[#E4EBF0] ">
-                <span>Stereo System</span>
-                <span className="text-[#8a97a4]">
-                  {yacht.data[0].attributes.technicalspec.stereo_system}
+            <HeroCarousel data={yacht?.data[0].attributes.image} />
+            <div className="pt-4 px-6 flex items-center justify-start md:justify-end gap-5 max-w-[1200px] mx-auto">
+              <h2 className="text-right">
+                <span className="text-xl text-primary">
+                  AED{yacht.data[0].attributes.price}/Day
                 </span>
-              </li>
-            )}
-            <li className="flex font-inter text-sm md:text-lg w-full items-center justify-between py-2 md:py-4  border-b border-[#E4EBF0] ">
-              <span>Make Year</span>
-              <span className="text-[#8a97a4]">
-                {yacht.data[0].attributes.technicalspec.make_year}
-              </span>
-            </li>
-            <li className="flex font-inter text-sm md:text-lg w-full items-center justify-between py-2 md:py-4  border-b border-[#E4EBF0] ">
-              <span>Speed</span>
-              <span className="text-[#8a97a4]">
-                {yacht.data[0].attributes.technicalspec.speed} knots
-              </span>
-            </li>
-            <li className="flex font-inter text-sm md:text-lg w-full items-center justify-between py-2 md:py-4  border-b border-[#E4EBF0] ">
-              <span>Minimum Hours</span>
-              <span className="text-[#8a97a4]">
-                {yacht.data[0].attributes.technicalspec.min_hours} hours
-              </span>
-            </li>
-          </ul>
-        </div>
+              </h2>
 
-        {yacht.data[0].attributes.onboard && (
-          <div>
-            <h2 className="inline-block mb-4 md:mb-8 font-inter text-primary font-semibold text-xl md:text-[40px]">
-              On Board
-            </h2>
-
-            <div>
-              <RichTextComponent bio={yacht.data[0].attributes.onboard} />
+              <AnimatedBtn
+                styles={"rounded-sm bg-primary text-white"}
+                text={"Book Now"}
+                msg={"Hi, I would like to know about your services."}
+              />
             </div>
           </div>
-        )}
+          <div className="max-w-[1200px] mx-auto py-10 md:py-16 px-6 flex flex-col gap-8 md:gap-16">
+            <div>
+              <div>
+                <h1 className="text-sm md:text-base text-justify md:text-left text-gray-500">
+                  {yacht.data[0].attributes.make.data.attributes.make}
+                </h1>
+                <span className="inline-block my-2 md:my-4 font-inter text-primary font-semibold text-2xl md:text-[40px]">
+                  {yacht.data[0].attributes.name}
+                </span>
+              </div>
 
-        <div>
-          <h2 className="inline-block mb-4 md:mb-8 font-inter text-primary font-semibold text-xl md:text-[40px]">
-            Amenities
-          </h2>
-          <Amenitys data={yacht.data[0].attributes.amenities} />
-        </div>
+              <RichTextComponent
+                bio={yacht.data[0].attributes.description}
+                style={
+                  "text-sm md:text-base text-justify md:text-left text-gray-500"
+                }
+              />
+            </div>
 
-        {/* <div>
+            <div>
+              <h2 className="inline-block mb-4 md:mb-8 font-inter text-primary font-semibold text-xl md:text-[40px]">
+                Destinations
+              </h2>
+
+              <RichTextComponent
+                bio={yacht.data[0].attributes.destinations}
+                style={
+                  "text-sm md:text-base text-justify md:text-left text-gray-500"
+                }
+              />
+            </div>
+
+            <div>
+              <h2 className="inline-block mb-4 md:mb-8 font-inter text-primary font-semibold text-xl md:text-[40px]">
+                Technical Specifications
+              </h2>
+              <ul className="grid grid-cols-1 md:grid-cols-2 gap-x-9  ">
+                <li className="flex font-inter text-sm md:text-lg w-full items-center justify-between py-2 md:py-4  border-b border-[#E4EBF0] ">
+                  <span>Make</span>
+                  <span className="text-[#8a97a4]">
+                    {yacht.data[0].attributes.make.data.attributes.make}
+                  </span>
+                </li>
+                <li className="flex font-inter text-sm md:text-lg w-full items-center justify-between py-2 md:py-4  border-b border-[#E4EBF0] ">
+                  <span>Model</span>
+                  <span className="text-[#8a97a4]">
+                    {yacht.data[0].attributes.model.data.attributes.model}
+                  </span>
+                </li>
+                <li className="flex font-inter text-sm md:text-lg w-full items-center justify-between py-2 md:py-4  border-b border-[#E4EBF0] ">
+                  <span>Length</span>
+                  <span className="text-[#8a97a4]">
+                    {yacht.data[0].attributes.technicalspec.length}
+                  </span>
+                </li>
+                <li className="flex font-inter text-sm md:text-lg w-full items-center justify-between py-2 md:py-4  border-b border-[#E4EBF0] ">
+                  <span>Capacity</span>
+                  <span className="text-[#8a97a4]">
+                    {`${
+                      yacht.data[0].attributes.technicalspec.capacity
+                    } People ${
+                      yacht.data[0].attributes.technicalspec.overnight
+                        ? `, ${yacht.data[0].attributes.technicalspec.overnight} overnight`
+                        : ""
+                    }`}
+                  </span>
+                </li>
+                <li className="flex font-inter text-sm md:text-lg w-full items-center justify-between py-2 md:py-4  border-b border-[#E4EBF0] ">
+                  <span>Category</span>
+                  <span className="text-[#8a97a4]">
+                    {yacht.data[0].attributes.technicalspec.category}
+                  </span>
+                </li>
+                <li className="flex font-inter text-sm md:text-lg w-full items-center justify-between py-2 md:py-4  border-b border-[#E4EBF0] ">
+                  <span>Rooms</span>
+                  <span className="text-[#8a97a4]">
+                    {yacht.data[0].attributes.technicalspec.rooms}
+                  </span>
+                </li>
+                {yacht.data[0].attributes.technicalspec.stereo_system && (
+                  <li className="flex font-inter text-sm md:text-lg w-full items-center justify-between py-2 md:py-4  border-b border-[#E4EBF0] ">
+                    <span>Stereo System</span>
+                    <span className="text-[#8a97a4]">
+                      {yacht.data[0].attributes.technicalspec.stereo_system}
+                    </span>
+                  </li>
+                )}
+                <li className="flex font-inter text-sm md:text-lg w-full items-center justify-between py-2 md:py-4  border-b border-[#E4EBF0] ">
+                  <span>Make Year</span>
+                  <span className="text-[#8a97a4]">
+                    {yacht.data[0].attributes.technicalspec.make_year}
+                  </span>
+                </li>
+                <li className="flex font-inter text-sm md:text-lg w-full items-center justify-between py-2 md:py-4  border-b border-[#E4EBF0] ">
+                  <span>Speed</span>
+                  <span className="text-[#8a97a4]">
+                    {yacht.data[0].attributes.technicalspec.speed} knots
+                  </span>
+                </li>
+                <li className="flex font-inter text-sm md:text-lg w-full items-center justify-between py-2 md:py-4  border-b border-[#E4EBF0] ">
+                  <span>Minimum Hours</span>
+                  <span className="text-[#8a97a4]">
+                    {yacht.data[0].attributes.technicalspec.min_hours} hours
+                  </span>
+                </li>
+              </ul>
+            </div>
+
+            {yacht.data[0].attributes.onboard && (
+              <div>
+                <h2 className="inline-block mb-4 md:mb-8 font-inter text-primary font-semibold text-xl md:text-[40px]">
+                  On Board
+                </h2>
+
+                <div>
+                  <RichTextComponent bio={yacht.data[0].attributes.onboard} />
+                </div>
+              </div>
+            )}
+
+            <div>
+              <h2 className="inline-block mb-4 md:mb-8 font-inter text-primary font-semibold text-xl md:text-[40px]">
+                Amenities
+              </h2>
+              <Amenitys data={yacht.data[0].attributes.amenities} />
+            </div>
+
+            {/* <div>
           <h2 className="inline-block mb-8 font-inter text-primary font-semibold text-xl md:text-[40px]">
             Gallery
           </h2>
@@ -227,11 +231,29 @@ export default async function YacthDetail({ params: { slug } }) {
             <GalleryJet />
           </div>
         </div> */}
-        <ContactForm
-          title={'Sail in Luxury'}
-          description={'Book your journey with our luxury yacht rentals now'}
-        />
-      </div>
+            <ContactForm
+              title={"Sail in Luxury"}
+              description={
+                "Book your journey with our luxury yacht rentals now"
+              }
+            />
+          </div>
+        </>
+      )}
     </main>
   );
 }
+
+export const generateStaticParams = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/yachts`, {
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_BEARER_TOKEN}`,
+    },
+  });
+
+  const data = await res.json();
+  console.log(data.data[0].attributes.slug);
+  return data.data.map((car) => ({
+    slug: car.attributes.slug,
+  }));
+};
