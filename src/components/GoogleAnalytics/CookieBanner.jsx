6567ccaa0cfc2b187ffeb9c11cf7ad63f05685ef
Rahul B <rahul.b@richylife.ae>
2023-09-11 09:46:24 +0400

    updated the cookie banner design

--- src/components/GoogleAnalytics/CookieBanner.jsx
+++ src/components/GoogleAnalytics/CookieBanner.jsx
@@ -1,5 +1,5 @@
 'use client';
-import Link from 'next/link';
+
 import { getLocalStorage, setLocalStorage } from '@/lib/storageHelper';
 import { useEffect, useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
@@ -47,18 +47,18 @@ export default function CookieBanner() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          className="my-10 mx-auto max-w-max flex md:max-w-screen-sm fixed bottom-10 z-30 left-0 right-0 px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4 bg-gray-700 rounded-lg shadow text-gray-200"
+          className="my-10 mx-4 md:mx-auto  max-w-max flex  flex-col md:max-w-screen-md fixed bottom-2 md:bottom-10 z-30 left-0 right-0 px-3 md:px-4 py-3 justify-between items-center  gap-4 bg-white rounded-lg shadow"
         >
-          <div className="text-center">
-            <Link href="/cookie-policy">
-              <p>
-                We use{' '}
-                <span className="font-bold text-sky-400">
-                  &nbsp;cookies&nbsp;
-                </span>
-                on our site{' '}
-              </p>
-            </Link>
+          <div className="mb-3">
+            <h2 className="text-sm md:text-base font-normal text-gray-600">
+              We use cookies to make your experience better
+            </h2>
+            <p className="text-xs md:text-sm font-light my-2 text-gray-500">
+              We use cookies to analyze your website usage, track visitor
+              numbers and sources, and enhance user-friendliness. Rest assured,
+              no personal info is collected. You can disable cookies in your
+              browser, but it may impact some site features
+            </p>
           </div>
           <div className="flex gap-2">
             <button
