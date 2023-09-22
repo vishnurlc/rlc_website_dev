a7838f3b0be8bdc6417a4fbd71d4ee02525e2d9d
Rahul B <rahul.b@richylife.ae>
2023-09-22 10:06:45 +0400

    Updated the title tags

--- src/app/luxury-yacht-rentals/page.jsx
+++ src/app/luxury-yacht-rentals/page.jsx
@@ -4,53 +4,49 @@ import {
   PictureGallery,
   Whyus,
   YachtListing,
-} from "@/components";
-import Faq from "@/components/home/faq";
-import React from "react";
+} from '@/components';
+import Faq from '@/components/home/faq';
+import React from 'react';
 
 export const metadata = {
   title: 'Luxury Yacht Rental Dubai',
   description:
     "Immerse yourself in luxury and style with Richylife Club's ultimate yacht rental experience in Dubai. Explore our fleet of exclusive yachts for unforgettable charters and sea adventures",
   keywords: [
-    "Luxury Yacht Rental Dubai",
-    "Exclusive Yacht Charters",
-    "Yacht Charter Services",
-    "Luxury Yacht Fleet",
-    "Private Yacht Hire",
-    "Yacht Charter Dubai",
-    "Yacht Rental Packages",
-    "Yacht Cruises",
-    "Luxury Yacht Tours",
-    "Private Yacht Experiences",
-    "Dubai Yacht Rentals",
-    "Yacht Charter Destinations",
-    "Yacht Rental Offers",
-    "Yacht Vacation Dubai",
-    "Luxury Yacht Events",
-    "Sea Adventures",
-    "Private Yacht Tours",
-    "Dubai yacht rental",
-    "Luxury yacht rental in Dubai",
-    "Private yacht rental Dubai",
-    "Yacht charter Dubai",
-    " Yacht rental deals Dubai",
-    "Yacht rental packages Dubai",
-    "Yacht rental for events Dubai",
-    " Yacht rental for weddings Dubai",
-    "Yacht rental for parties Dubai",
-    "Yacht rental for corporate events Dubai",
-    "Yacht rental for photoshoots Dubai",
+    'Luxury Yacht Rental Dubai',
+    'Exclusive Yacht Charters',
+    'Yacht Charter Services',
+    'Luxury Yacht Fleet',
+    'Private Yacht Hire',
+    'Yacht Charter Dubai',
+    'Yacht Rental Packages',
+    'Yacht Cruises',
+    'Luxury Yacht Tours',
+    'Private Yacht Experiences',
+    'Dubai Yacht Rentals',
+    'Yacht Charter Destinations',
+    'Yacht Rental Offers',
+    'Yacht Vacation Dubai',
+    'Luxury Yacht Events',
+    'Sea Adventures',
+    'Private Yacht Tours',
+    'Dubai yacht rental',
+    'Luxury yacht rental in Dubai',
+    'Private yacht rental Dubai',
+    'Yacht charter Dubai',
+    ' Yacht rental deals Dubai',
+    'Yacht rental packages Dubai',
+    'Yacht rental for events Dubai',
+    ' Yacht rental for weddings Dubai',
+    'Yacht rental for parties Dubai',
+    'Yacht rental for corporate events Dubai',
+    'Yacht rental for photoshoots Dubai',
   ],
   openGraph: {
-<<<<<<< HEAD
     title: 'Luxury Yacht Rental Dubai',
-=======
-    title: "Luxury Yacht Rental Dubai | Exclusive Yacht Charters",
->>>>>>> 2d4f94d3e3c14a75c578ccfd9cb043df7dc90219
     description:
       "Immerse yourself in luxury and style with Richylife Club's ultimate yacht rental experience in Dubai. Explore our fleet of exclusive yachts for unforgettable charters and sea adventures",
-    siteName: "Richylife Club",
+    siteName: 'Richylife Club',
     images: [
       {
         url: `${process.env.WEB_URL}/assets/privateyachts/opengraphimage.png`,
@@ -63,8 +59,8 @@ export const metadata = {
         height: 200,
       },
     ],
-    locale: "en_US",
-    type: "website",
+    locale: 'en_US',
+    type: 'website',
   },
 };
 
@@ -84,7 +80,7 @@ export async function getData() {
 
     return data;
   } catch (error) {
-    console.log("s", error);
+    console.log('s', error);
     return {};
   }
 }
@@ -95,17 +91,17 @@ export default async function page() {
     <main>
       <h1 className="sr-only ">Luxury Yacht Rentals - Richylife Club</h1>
       <HeroSection2
-        type={"image"}
-        heading1={"Luxury Yacht"}
-        heading2={"Rental in Dubai"}
-        subheading={"Experience the sea breeze in luxury"}
-        posterurl={"/assets/privateyachts/banner.png"}
-        btntext={"Book your trip"}
-        url={"/assets/privateyachts/banner.png"}
+        type={'image'}
+        heading1={'Luxury Yacht'}
+        heading2={'Rental in Dubai'}
+        subheading={'Experience the sea breeze in luxury'}
+        posterurl={'/assets/privateyachts/banner.png'}
+        btntext={'Book your trip'}
+        url={'/assets/privateyachts/banner.png'}
         overlay={1}
       />
       <div className="mx-auto max-w-[1200px]">
-        <YachtListing data={yachts} variant={"yacht"} />
+        <YachtListing data={yachts} variant={'yacht'} />
       </div>
       {/* <div className=" my-16 flex flex-col gap-9 md:gap-16">
         <PictureGallery
@@ -118,11 +114,11 @@ export default async function page() {
       <div className="my-9 md:my-16 px-6 max-w-[1200px] mx-auto">
         <Whyus />
       </div>
-      <Faq category={"yacht"} />
+      <Faq category={'yacht'} />
       <div className="my-9 md:my-16 px-6">
         <ContactForm
-          title={"Set Sailing Now"}
-          description={"Book your adventurous Journey with us"}
+          title={'Set Sailing Now'}
+          description={'Book your adventurous Journey with us'}
         />
       </div>
     </main>
