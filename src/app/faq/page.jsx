a2dd9be876735c4c0999cbc51bbe92bb13382cc6
Rahul B <rahul.b@richylife.ae>
2023-09-23 12:40:07 +0400

    faq page made to server rendering

--- src/app/faq/page.jsx
+++ src/app/faq/page.jsx
@@ -1,54 +1,42 @@
-'use client';
-import { FilterBtn, Loader, SectionHeading } from '@/components';
-import FaqAccordation from '@/components/ui/accordation/accordation';
-import React, { Suspense, useEffect, useState } from 'react';
-
-async function getData({ value }) {
-  try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/faq-categories?filters[category][$eq]=${value}&populate=*`,
+import { SectionHeading } from '@/components';
+import FaqComponent from '@/components/faq/FaqComponent';
+export const metadata = {
+  title: 'Frequently Asked Questions',
+  description:
+    'Do you have questions ? No worries! all the question related to dubai, luxury auto rentals , yachts, parties, luxury lifestyles are answered here.',
+  keywords: [
+    'Richylife Club',
+    'Luxury Experiences Dubai',
+    'Contact Information',
+    'Inquiries',
+    'Reservations',
+    'Luxury Lifestyle',
+    'Exclusive Experiences',
+    'Dubai UAE',
+    'Premium Leisure',
+    'Bespoke Services',
+    'Dubai Holidays',
+    'VIP Support',
+  ],
+  openGraph: {
+    title: 'Frequently Asked Questions',
+    description:
+      'Do you have questions ? No worries! all the question related to dubai, luxury auto rentals , yachts, parties, luxury lifestyles are answered here.',
+    siteName: 'Richylife Club',
+    images: [
       {
-        next: { revalidate: 40 },
-        headers: {
-          Authorization: `Bearer ${process.env.NEXT_PUBLIC_BEARER_TOKEN}`,
-        },
-      }
-    );
-
-    const data = await res.json();
-
-    return data;
-  } catch (error) {
-    console.log('s', error);
-    return {};
-  }
-}
-
-const categoryData = [
-  {
-    value: 'Luxury Car Rental',
-    category: 'car',
+        url: `${process.env.WEB_URL}/`,
+        width: 800,
+        height: 600,
+        alt: 'Richylife Club Luxury Experiences in Dubai',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
   },
-  { value: 'Luxury Yacht Rental', category: 'yacht' },
-  { value: 'Exclusive Water Sports', category: 'jetski' },
-  { value: 'Exotic Pet VIP Experience', category: 'pet' },
-  { value: 'Chauffer Service', category: 'chauffer' },
-  { value: 'Helicopter Charters', category: 'helicopter' },
-  { value: 'Private Jet Charters', category: 'privatejet' },
-  { value: 'Premium Desert Adventure', category: 'desertsafari' },
-];
+};
 
 const FaqPage = () => {
-  const [category, setCategory] = useState('car');
-  const [data, setData] = useState();
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getData({ value: category });
-      setData(data.data);
-    };
-    fetchData();
-  }, [category]);
-
   return (
     <main className="w-full overflow-hidden">
       <div className="mt-[120px]  px-6 md:px-16">
@@ -56,23 +44,7 @@ const FaqPage = () => {
           title={'FAQ'}
           description={'All your concerns are answered'}
         />
-        <div className="my-8 max-w-[1200px] mx-auto">
-          <FilterBtn
-            items={categoryData}
-            category={category}
-            setCategory={setCategory}
-          />
-        </div>
-
-        <div className="max-w-[1200px] mx-auto ">
-          <div className="mt-12 min-h-[50vh] ">
-            {data ? (
-              <FaqAccordation data={data[0]?.attributes.faqs.data} />
-            ) : (
-              <Loader />
-            )}
-          </div>
-        </div>
+        <FaqComponent />
       </div>
     </main>
   );
---
+++ src/components/faq/FaqComponent.jsx
@@ -0,0 +1,73 @@
+'use client';
+import { FilterBtn, Loader } from '@/components';
+import FaqAccordation from '@/components/ui/accordation/accordation';
+import React, { useEffect, useState } from 'react';
+
+async function getData({ value }) {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/faq-categories?filters[category][$eq]=${value}&populate=*`,
+      {
+        next: { revalidate: 40 },
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_BEARER_TOKEN}`,
+        },
+      }
+    );
+
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    console.log('s', error);
+    return {};
+  }
+}
+const categoryData = [
+  {
+    value: 'Luxury Car Rental',
+    category: 'car',
+  },
+  { value: 'Luxury Yacht Rental', category: 'yacht' },
+  { value: 'Exclusive Water Sports', category: 'jetski' },
+  { value: 'Exotic Pet VIP Experience', category: 'pet' },
+  { value: 'Chauffer Service', category: 'chauffer' },
+  { value: 'Helicopter Charters', category: 'helicopter' },
+  { value: 'Private Jet Charters', category: 'privatejet' },
+  { value: 'Premium Desert Adventure', category: 'desertsafari' },
+];
+const FaqComponent = () => {
+  const [category, setCategory] = useState('car');
+  const [data, setData] = useState();
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getData({ value: category });
+      setData(data.data);
+    };
+    fetchData();
+  }, [category]);
+
+  return (
+    <>
+      <div className="my-8 max-w-[1200px] mx-auto">
+        <FilterBtn
+          items={categoryData}
+          category={category}
+          setCategory={setCategory}
+        />
+      </div>
+
+      <div className="max-w-[1200px] mx-auto ">
+        <div className="mt-12 min-h-[50vh] ">
+          {data ? (
+            <FaqAccordation data={data[0]?.attributes.faqs.data} />
+          ) : (
+            <Loader />
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FaqComponent;
