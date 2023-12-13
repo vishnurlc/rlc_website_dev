0691e123f3421582356a3b1863c18df3516b0c2a
Rahul B <rahul.b@richylife.ae>
2023-12-13 12:43:50 +0400

    Added pixel code

--- src/components/rootLayout/RootLayout.jsx
+++ src/components/rootLayout/RootLayout.jsx
@@ -35,7 +35,33 @@ const RootLayout = ({ children }) => {
    })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTAG_ID}');
         `}
       </Script>
+      <Script
+        id="fb-pixelcode"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '374541755133413');
+          fbq('track', 'PageView');
+`,
+        }}
+      />
       <body>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style="display:none"
+            src="https://www.facebook.com/tr?id=374541755133413&ev=PageView&noscript=1"
+          />
+        </noscript>
         <CurrencyProvider>
           <Header />
           {children}
