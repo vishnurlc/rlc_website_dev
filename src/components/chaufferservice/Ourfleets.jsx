4ad0aa5c7d1ba10b25af44533f9e2845940628e6
vishnu_p <vishnu.p@richylife.ae>
2023-12-30 10:05:14 +0400

    chauffer card

--- src/components/chaufferservice/Ourfleets.jsx
+++ src/components/chaufferservice/Ourfleets.jsx
@@ -9,6 +9,7 @@ import { FaPhoneAlt } from "react-icons/fa";
 import { AiOutlineExclamationCircle } from "react-icons/ai";
 import { useCurrency } from "@/context/currencyContext";
 import { usePathname } from "next/navigation";
+import Motionslider from "../ui/slidermotion/motionslider";
 
 const jetski = [
   {
@@ -50,37 +51,42 @@ const Ourfleets = () => {
   return (
     <div>
       <SectionHeading title={`Our fleets`} />
-      <div className="mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-8">
-        {jetski.map((item, index) => (
-          <div className="relative  w-full aspect-[357/406]" key={index}>
-            <Link href={`/chauffeur-service/ss?body=${item.link}`}>
-              <Image
-                src={item.image}
-                alt={`${item.name} rental | Richy life Club`}
-                fill
-                style={{
-                  objectFit: "cover",
-                }}
-              />
-            </Link>
-            <div className="absolute p-4 text-white w-full h-fit bg-black bg-opacity-60 bottom-0 left-0 right-0 z-10">
-              <div>
-                <h2 className="uppercase font-medium tracking-wide text-xl font-poppins">
-                  {item.name}
-                </h2>
-                {item.price && (
-                  <span className="text-gray-400 text-sm">
-                    Starting from {convertPrice(item.price)}
-                  </span>
-                )}
-              </div>
+      <div className="mt-7">
+        <Motionslider>
+          {jetski.map((item, index) => (
+            <div
+              className="relative  w-full min-w-[227px] aspect-[357/406] rounded-md overflow-hidden"
+              key={index}
+            >
+              <Link href={`/chauffeur-service/ss?body=${item.link}`}>
+                <Image
+                  src={item.image}
+                  alt={`${item.name} rental | Richy life Club`}
+                  fill
+                  style={{
+                    objectFit: "cover",
+                  }}
+                />
+              </Link>
+              <div className="absolute p-4 text-white w-full h-fit  bottom-0 left-0 right-0 z-10">
+                <div>
+                  <h2 className="uppercase font-medium tracking-wide text-sm md:text-xl font-poppins">
+                    {item.name}
+                  </h2>
+                  {item.price && (
+                    <span className="text-gray-400 text-sm">
+                      {/* Starting from {convertPrice(item.price)} */}
+                    </span>
+                  )}
+                </div>
 
-              <Button className="mt-4 px-4 py-2 bg-gold text-white rounded-sm">
-                Book Now
-              </Button>
+                {/* <Button className="text-sm mt-4 md:px-4 py-2 bg-gold text-white rounded-sm">
+                  Book Now
+                </Button> */}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </Motionslider>
       </div>
     </div>
   );
