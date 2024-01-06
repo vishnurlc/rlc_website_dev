56bd490446531cb2cd45384f7ae48653cae29286
vishnu_p <vishnu.p@richylife.ae>
2024-01-06 13:48:03 +0400

    hotel detail page

--- src/app/hotels/[slug]/page.jsx
+++ src/app/hotels/[slug]/page.jsx
@@ -1,46 +1,54 @@
-import { ContactForm, HeroCarousel, PriceComponent } from '@/components';
-import GoogleMapComponent from '@/components/mapComponent/GoogleMapComponent';
+import { ContactForm, HeroCarousel, PriceComponent } from "@/components";
+import GoogleMapComponent from "@/components/mapComponent/GoogleMapComponent";
 
-import AnimatedBtn from '@/components/premiumjetski/AnimatedBtn';
-import Link from 'next/link';
-import { FaLocationDot } from 'react-icons/fa6';
+import AnimatedBtn from "@/components/premiumjetski/AnimatedBtn";
+import Link from "next/link";
+import { FaLocationDot } from "react-icons/fa6";
+import { MdPerson2 } from "react-icons/md";
 
-export const metadata = {
-  title: 'Luxury Chauffeur Service in Dubai',
-  description:
-    'Book a luxury chauffeur service in Dubai for your airport transfers, half and full day car rentals, full Dubai tours, or events pickup and drop',
-  keywords: [
-    'chauffeur service dubai',
-    'luxury chauffeur service dubai',
-    ' airport transfers dubai',
-    'car rentals dubai',
-    'full dubai tours',
-    'events pickup and drop dubai',
-    'Richy Life Club chauffer service',
-    'richylife club chauffer service',
-  ],
-
-  openGraph: {
-    title: 'Luxury Chauffeur Service in Dubai',
-    description:
-      'Book a luxury chauffeur service in Dubai for your airport transfers, half and full day car rentals, full Dubai tours, or events pickup and drop',
-    siteName: 'Richy life Club',
-    images: [
+export async function generateMetadata({ params }) {
+  try {
+    const car = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/club-packages?filters[slug][$eq]=${params.slug}&populate=image`,
       {
-        url: `${process.env.NEXT_PUBLIC_WEB_URL}/assets/chauffeur/hero.png`,
-        width: 800,
-        height: 600,
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_BEARER_TOKEN}`,
+        },
+      }
+    ).then((res) => res.json());
+
+    return {
+      title:
+        car.data[0].attributes.name || "| Luxury Clubs & Restaurants in Dubai",
+      description:
+        car.data[0].attributes.description ||
+        " Luxury Clubs & Restaurants in Dubai",
+      openGraph: {
+        type: "website",
+        title:
+          car.data[0].attributes.name ||
+          "| Luxury Clubs & Restaurants in Dubai",
+        description:
+          car.data[0].attributes.description ||
+          "Luxury Clubs & Restaurants in Dubai",
+        images: [
+          {
+            url: `${car.data[0].attributes.image.data[0].attributes.url}`,
+            width: 800,
+            height: 600,
+          },
+          {
+            url: `${car.data[0].attributes.image.data[0].attributes.url}`,
+            width: 300,
+            height: 200,
+          },
+        ],
       },
-      {
-        url: `${process.env.NEXT_PUBLIC_WEB_URL}/assets/chauffeur/hero.png`,
-        width: 300,
-        height: 200,
-      },
-    ],
-    locale: 'en_US',
-    type: 'website',
-  },
-};
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 async function getData(slug) {
   let api = `${process.env.NEXT_PUBLIC_BACKEND_URL}/club-packages?filters[slug][$eq]=${slug.slug}&populate=*`;
@@ -64,11 +72,10 @@ const page = async ({ params }) => {
   const car = await getData(params);
 
   return (
-    <main>
-      <div className="mt-[100px]">
+    <main className="pt-[108px] md:pt-[128px]">
+      <div>
         <HeroCarousel
           data={car.data[0].attributes.image}
-          fgo
           name={car.data[0].attributes.name}
         />
         <div className="pt-4 px-6 flex items-center justify-start md:justify-end gap-5 max-w-[1200px] mx-auto">
@@ -80,9 +87,9 @@ const page = async ({ params }) => {
           </h2>
 
           <AnimatedBtn
-            styles={'rounded-md bg-gold text-white'}
-            text={'Reserve Now'}
-            msg={'Hi, I would like to know about your services.'}
+            styles={"rounded-md bg-gold text-white"}
+            text={"Reserve Now"}
+            msg={"Hi, I would like to know about your services."}
           />
         </div>
       </div>
@@ -96,33 +103,117 @@ const page = async ({ params }) => {
                 target="_blank"
                 rel="noreferrer"
               >
-                <FaLocationDot color="green" /> &nbsp;{' '}
+                <FaLocationDot color="green" /> &nbsp;{" "}
                 {car.data[0].attributes.place}
               </Link>
             </h1>
             <span className=" inline-block my-2 md:my-4 font-inter text-primary font-semibold text-2xl md:text-[40px]">
               {car.data[0].attributes.name}
             </span>
+
             <p className="text-sm md:text-base text-justify md:text-left text-gray-500">
               {car.data[0].attributes.description}
             </p>
           </div>
           <div className="max-w-[1200px] mx-auto py-10 md:py-16 px-6 flex flex-col gap-8 md:gap-16">
             {/* product detail */}
+            <div>
+              <div className="w-full h-full ">
+                <div className="grid grid-cols-1 md:grid-cols-2 max-w-[1200px] mx-auto gap-8">
+                  <div className="border w-full  rounded-lg shadow-lg">
+                    <div className="flex flex-col items-center md:items-start px-6 py-4">
+                      <div className="flex flex-col w-fit">
+                        <h2 className="font-bold text-xl ">King Room</h2>
+                        <div className="border-2 border-gray-300 mb-3 text"></div>
+                      </div>
+                      <div className="flex">
+                        <MdPerson2 />
+                        <MdPerson2 />
+                      </div>
+                      <div className="text-gray-500  text-center md:text-start">
+                        <p>
+                          Average spending Per Person :{" "}
+                          <span>
+                            <PriceComponent
+                              cost={car.data[0].attributes.avg_price_per_person}
+                            />
+                          </span>
+                        </p>
+                      </div>
+                      <div className="text-gray-500 mt-7  text-center md:text-start">
+                        <p className="text-primary text-lg">Cuisines</p>
+                        <p>
+                          {car.data[0].attributes.restuarant_cuisines.data.map(
+                            (item, index, array) => (
+                              <span key={index}>
+                                {item.attributes.cuisine}
+                                {index < array.length - 1 && ","}{" "}
+                              </span>
+                            )
+                          )}
+                        </p>
+                      </div>
+                      <div className="text-gray-500 mt-7  text-center md:text-start flex items-center justify-start gap-2 flex-wrap">
+                        {car.data[0].attributes.restaurant_types.data.map(
+                          (item, index) => (
+                            <p
+                              className=" px-4 py-2 rounded-full border border-gray-300"
+                              key={index}
+                            >
+                              {item.attributes.type}
+                            </p>
+                          )
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="border w-full  rounded-lg shadow-lg">
+                    <div className="px-6 py-4">
+                      <div className="flex flex-col w-24">
+                        <h2 className="font-bold text-xl">Location</h2>
+                        <div className="border-2 border-gray-300 mb-3 text"></div>
+                      </div>
+                      <div>
+                        <GoogleMapComponent
+                          location={car.data[0].attributes.embeddedsrcmap}
+                        />
+                      </div>
+                      <div className="mt-6">
+                        <h3 className="text-sm md:text-base text-justify md:text-left text-gray-500 ">
+                          <Link
+                            href={car.data[0].attributes.locationlink}
+                            className="flex items-center"
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            <FaLocationDot color="green" /> &nbsp;{" "}
+                            {car.data[0].attributes.place}
+                          </Link>
+                        </h3>
 
+                        <AnimatedBtn
+                          text={"Reserve now"}
+                          styles={"rounded-md bg-gold text-white mt-4 "}
+                          msg={`I would like to reserve a table at ${car.data[0].attributes.name}`}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* location and other */}
             <div className="w-full h-full ">
               <div className="grid grid-cols-1 md:grid-cols-2 max-w-[1200px] mx-auto gap-8">
                 <div className="border w-full  rounded-lg shadow-lg">
                   <div className="flex flex-col items-center md:items-start px-6 py-4">
                     <div className="flex flex-col w-24">
-                      <h2 className="font-bold text-xl dark:text-gray-100">
-                        Details
-                      </h2>
+                      <h2 className="font-bold text-xl ">Details</h2>
                       <div className="border-2 border-gray-300 mb-3 text"></div>
                     </div>
-                    <div className="text-gray-500 dark:text-gray-200 text-center md:text-start">
+                    <div className="text-gray-500  text-center md:text-start">
                       <p>
-                        Average spending Per Person :{' '}
+                        Average spending Per Person :{" "}
                         <span>
                           <PriceComponent
                             cost={car.data[0].attributes.avg_price_per_person}
@@ -130,20 +221,20 @@ const page = async ({ params }) => {
                         </span>
                       </p>
                     </div>
-                    <div className="text-gray-500 mt-7 dark:text-gray-200 text-center md:text-start">
+                    <div className="text-gray-500 mt-7  text-center md:text-start">
                       <p className="text-primary text-lg">Cuisines</p>
                       <p>
-                        {car.data[0].attributes.Cuisines.map(
+                        {car.data[0].attributes.restuarant_cuisines.data.map(
                           (item, index, array) => (
                             <span key={index}>
-                              {item.cuisine}
-                              {index < array.length - 1 && ','}{' '}
+                              {item.attributes.cuisine}
+                              {index < array.length - 1 && ","}{" "}
                             </span>
                           )
                         )}
                       </p>
                     </div>
-                    <div className="text-gray-500 mt-7 dark:text-gray-200 text-center md:text-start flex items-center justify-start gap-2 flex-wrap">
+                    <div className="text-gray-500 mt-7  text-center md:text-start flex items-center justify-start gap-2 flex-wrap">
                       {car.data[0].attributes.restaurant_types.data.map(
                         (item, index) => (
                           <p
@@ -160,9 +251,7 @@ const page = async ({ params }) => {
                 <div className="border w-full  rounded-lg shadow-lg">
                   <div className="px-6 py-4">
                     <div className="flex flex-col w-24">
-                      <h2 className="font-bold text-xl dark:text-gray-100">
-                        Location
-                      </h2>
+                      <h2 className="font-bold text-xl">Location</h2>
                       <div className="border-2 border-gray-300 mb-3 text"></div>
                     </div>
                     <div>
@@ -178,14 +267,14 @@ const page = async ({ params }) => {
                           target="_blank"
                           rel="noreferrer"
                         >
-                          <FaLocationDot color="green" /> &nbsp;{' '}
+                          <FaLocationDot color="green" /> &nbsp;{" "}
                           {car.data[0].attributes.place}
                         </Link>
                       </h3>
 
                       <AnimatedBtn
-                        text={'Reserve now'}
-                        styles={'rounded-md bg-gold text-white mt-4 '}
+                        text={"Reserve now"}
+                        styles={"rounded-md bg-gold text-white mt-4 "}
                         msg={`I would like to reserve a table at ${car.data[0].attributes.name}`}
                       />
                     </div>
@@ -195,8 +284,8 @@ const page = async ({ params }) => {
             </div>
 
             <ContactForm
-              title={'Experience the ultimate Luxury'}
-              description={'Book your journey with our luxury car rentals now'}
+              title={"Experience the ultimate Luxury"}
+              description={"Book your journey with our luxury car rentals now"}
             />
           </div>
         </div>
@@ -206,3 +295,23 @@ const page = async ({ params }) => {
 };
 
 export default page;
+
+export const generateStaticParams = async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/club-packages`,
+    {
+      next: {
+        revalidate: 40,
+      },
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_BEARER_TOKEN}`,
+      },
+    }
+  );
+
+  const data = await res.json();
+
+  return data.data.map((blog) => ({
+    slug: blog.attributes.slug,
+  }));
+};
--- src/components/club/ClubCard.jsx
+++ src/components/club/ClubCard.jsx
@@ -1,27 +1,27 @@
-'use client';
-import AnimatedBtn from '../premiumjetski/AnimatedBtn';
-import Image from 'next/image';
-import Link from 'next/link';
-import { FaPhoneAlt } from 'react-icons/fa';
-import { RiWhatsappFill } from 'react-icons/ri';
-import { Button } from '../ui/button/Button';
+"use client";
+import AnimatedBtn from "../premiumjetski/AnimatedBtn";
+import Image from "next/image";
+import Link from "next/link";
+import { FaPhoneAlt } from "react-icons/fa";
+import { RiWhatsappFill } from "react-icons/ri";
+import { Button } from "../ui/button/Button";
 
 import {
   MdAccessTime,
   MdOutlineLocationOn,
   MdOutlinePersonOutline,
-} from 'react-icons/md';
-import { IoAirplaneOutline } from 'react-icons/io5';
+} from "react-icons/md";
+import { IoAirplaneOutline } from "react-icons/io5";
 
-import { useCurrency } from '@/context/currencyContext';
+import { useCurrency } from "@/context/currencyContext";
 const ClubCard = ({ data, order }) => {
   const { selectedCurrency, conversionRates } = useCurrency();
   const convertPrice = (price) => {
     const rate = conversionRates.rates[selectedCurrency];
 
     const amt = Math.round(Number(price) * rate);
-    const priceFormatted = new Intl.NumberFormat('ae', {
-      style: 'currency',
+    const priceFormatted = new Intl.NumberFormat("ae", {
+      style: "currency",
       currency: selectedCurrency,
       minimumFractionDigits: 0, // Set minimumFractionDigits to 0
       maximumFractionDigits: 0, // Set maximumFractionDigits to 0
@@ -30,40 +30,6 @@ const ClubCard = ({ data, order }) => {
   };
 
   return (
-    // <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-9 md:my-20">
-    //   <div className={`order-${order}`}>
-    //     <div className="text-white mb-7">
-    //       <h2 className=" text-2xl md:text-4xl mb-6">{item.attributes.name}</h2>
-    //       <p className="text-sm md:text-xl text-gray-400 pb-5">
-    //         {item.attributes.price}
-    //       </p>
-    //       <p className="text-gray-500 text-sm md:text-base tracking-wide ">
-    //         {item.attributes.description}
-    //       </p>
-    //       <p className="text-gray-500 text-sm md:text-base tracking-wide ">
-    //         {/* {item.attributes.place} */}
-    //       </p>
-    //     </div>
-    //     <AnimatedBtn
-    //       text={"Reservce Now"}
-    //       styles={"bg-gold text-white rounded-sm"}
-    //       msg={`I'm writing to you today to inquire about the ${item.attributes.name}. I'm interested in learning more about its specifications, price, and availability.`}
-    //     />
-    //   </div>
-    //   <div className="relative w-full h-full min-h-[350px] overflow-hidden rounded">
-    //     <Image
-    //       src={item.attributes.image.data[0].attributes.url}
-    //       alt="Premium Desert Safari Packages | Richy life Club "
-    //       fill
-    //       style={{
-    //         objectFit: "cover",
-    //         objectPosition: "50%",
-    //       }}
-    //       izes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-    //     />
-    //     <div className="bg-black bg-opacity-10 absolute inset-0"></div>
-    //   </div>
-    // </div>
     <div className="grid grid-cols-1 w-full bg-[#fbfbfb] lg:grid-cols-5  max-w-[1200px] rounded-sm overflow-hidden mx-auto bg-white] mb-5">
       <Link
         href={`/clubs-restaurants/${data.attributes.slug}`}
@@ -74,8 +40,8 @@ const ClubCard = ({ data, order }) => {
           alt={`Rent ${data.attributes.name} with Richy life Club`}
           fill
           style={{
-            objectFit: 'cover',
-            objectPosition: 'center',
+            objectFit: "cover",
+            objectPosition: "center",
           }}
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 80vw"
           id="selectDisable"
@@ -88,11 +54,7 @@ const ClubCard = ({ data, order }) => {
               {data.attributes.name}
             </Link>
           </h3>
-
-          {/* <p>{data.attributes.description}</p> */}
-
           <CarDetail data={data} />
-
           <div className="flex flex-col md:flex-row items-start md:items-center justify-between w-full py-5 gap-6">
             <div className="w-full md:w-fit">
               <span className="text-primary font-normal text-2xl">
@@ -142,8 +104,8 @@ function CarDetail({ data }) {
     const rate = conversionRates.rates[selectedCurrency];
 
     const amt = Math.round(Number(price) * rate);
-    const priceFormatted = new Intl.NumberFormat('ae', {
-      style: 'currency',
+    const priceFormatted = new Intl.NumberFormat("ae", {
+      style: "currency",
       currency: selectedCurrency,
       minimumFractionDigits: 0, // Set minimumFractionDigits to 0
       maximumFractionDigits: 0, // Set maximumFractionDigits to 0
@@ -164,8 +126,8 @@ function CarDetail({ data }) {
       </div>
       <div className="px-2 py-2 bg-slate-100 flex items-center justify-center">
         <div className="text-primary flex items-center gap-2 text-sm md:text-base font-medium leading-tight">
-          {'$'.repeat(data.attributes.dollarcountmin)} -{' '}
-          {'$'.repeat(data.attributes.dollarcountmax)}
+          {"$".repeat(data.attributes.dollarcountmin)} -{" "}
+          {"$".repeat(data.attributes.dollarcountmax)}
         </div>
       </div>
     </div>
