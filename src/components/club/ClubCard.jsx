e0b3f16ea0ab0be8f44c137634e04f0543161378
Rahul B <rahul.b@richylife.ae>
2024-01-11 18:28:53 +0400

    Club card update

--- src/components/club/ClubCard.jsx
+++ src/components/club/ClubCard.jsx
@@ -1,27 +1,27 @@
-"use client";
-import AnimatedBtn from "../premiumjetski/AnimatedBtn";
-import Image from "next/image";
-import Link from "next/link";
-import { FaPhoneAlt } from "react-icons/fa";
-import { RiWhatsappFill } from "react-icons/ri";
-import { Button } from "../ui/button/Button";
+'use client';
+import AnimatedBtn from '../premiumjetski/AnimatedBtn';
+import Image from 'next/image';
+import Link from 'next/link';
+import { FaPhoneAlt } from 'react-icons/fa';
+import { RiWhatsappFill } from 'react-icons/ri';
+import { Button } from '../ui/button/Button';
 
 import {
   MdAccessTime,
   MdOutlineLocationOn,
   MdOutlinePersonOutline,
-} from "react-icons/md";
-import { IoAirplaneOutline } from "react-icons/io5";
+} from 'react-icons/md';
+import { IoAirplaneOutline } from 'react-icons/io5';
 
-import { useCurrency } from "@/context/currencyContext";
+import { useCurrency } from '@/context/currencyContext';
 const ClubCard = ({ data, order }) => {
   const { selectedCurrency, conversionRates } = useCurrency();
   const convertPrice = (price) => {
     const rate = conversionRates.rates[selectedCurrency];
 
     const amt = Math.round(Number(price) * rate);
-    const priceFormatted = new Intl.NumberFormat("ae", {
-      style: "currency",
+    const priceFormatted = new Intl.NumberFormat('ae', {
+      style: 'currency',
       currency: selectedCurrency,
       minimumFractionDigits: 0, // Set minimumFractionDigits to 0
       maximumFractionDigits: 0, // Set maximumFractionDigits to 0
@@ -42,8 +42,8 @@ const ClubCard = ({ data, order }) => {
           alt={`Rent ${data.attributes.name} with Richy life Club`}
           fill
           style={{
-            objectFit: "cover",
-            objectPosition: "center",
+            objectFit: 'cover',
+            objectPosition: 'center',
           }}
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 80vw"
           id="selectDisable"
@@ -60,9 +60,9 @@ const ClubCard = ({ data, order }) => {
           <div className="flex flex-col md:flex-row items-start md:items-center justify-between w-full py-5 gap-6">
             <div className="w-full md:w-fit">
               <span className="text-primary font-normal text-2xl">
-                {convertPrice(data.attributes.avg_price_per_person)}
+                {/* {convertPrice(data.attributes.avg_price_per_person)} */}
                 <span className="text-secondary text-sm font-normal">
-                  /Person
+                  {/* /Person */}
                 </span>
               </span>
             </div>
@@ -89,7 +89,7 @@ const ClubCard = ({ data, order }) => {
                 action={() => setBookingOpen(true)}
                 msg={`I would like to know more about ${data.attributes.name} booking`}
               >
-                Book Now
+                Reserve Now
               </Button>
             </div>
           </div>
@@ -115,8 +115,8 @@ function CarDetail({ data }) {
       </div>
       <div className="px-2 py-2 bg-slate-100 flex items-center justify-center">
         <div className="text-primary flex items-center gap-2 text-sm md:text-base font-medium leading-tight">
-          {"$".repeat(data.attributes.dollarcountmin)} -{" "}
-          {"$".repeat(data.attributes.dollarcountmax)}
+          {'$'.repeat(data.attributes.dollarcountmin)} -{' '}
+          {'$'.repeat(data.attributes.dollarcountmax)}
         </div>
       </div>
       {data.attributes.restaurant_types.data?.map((e, index) => (
