853a8543b399c1bf3d0cdfabd7a709a46101ad5e
Rahul B <rahul.b@richylife.ae>
2024-01-22 11:27:34 +0400

    Currency debug fixed

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,32 +1,32 @@
-import RootLayout from "@/components/rootLayout/RootLayout";
-import "./globals.css";
-import { Inter, Poppins, Nunito_Sans } from "next/font/google";
+import RootLayout from '@/components/rootLayout/RootLayout';
+import './globals.css';
+import { Inter, Poppins, Nunito_Sans } from 'next/font/google';
 
 export const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
-  weight: ["200", "300", "400"],
-  variable: "--font-inter",
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['200', '300', '400'],
+  variable: '--font-inter',
 });
 
 export const nunito_sans = Nunito_Sans({
-  subsets: ["latin"],
-  display: "swap",
-  weight: ["500", "600", "700", "800"],
-  variable: "--font-nunito",
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['500', '600', '700', '800'],
+  variable: '--font-nunito',
 });
 export const poppins = Poppins({
-  subsets: ["latin"],
-  display: "swap",
-  weight: ["400", "500", "600"],
-  variable: " --font-poppins",
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400', '500', '600'],
+  variable: ' --font-poppins',
 });
 
 export const metadata = {
   metadataBase: new URL(`${process.env.NEXT_PUBLIC_WEB_URL}`),
   title: {
-    default: "Richy Life Club ",
-    template: "%s | Richy life Club",
+    default: 'Richy Life Club ',
+    template: '%s | Richy life Club',
   },
 };
 
@@ -37,7 +37,7 @@ export default function Layout({ children, params }) {
       lang="en"
       className={`${inter.variable} ${nunito_sans.variable} ${poppins.variable}`}
     >
-      <RootLayout lang={lang}>{children}</RootLayout>
+      <RootLayout lang={'en'}>{children}</RootLayout>
     </html>
   );
 }
