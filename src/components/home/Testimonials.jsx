b290e46ec4ddf30851d1e9bcbc6b97ff2b6a0500
vishnu_p <vishnu.p@richylife.ae>
2024-01-29 15:22:13 +0400

    tesimonials change to gallery

--- src/components/home/Testimonials.jsx
+++ src/components/home/Testimonials.jsx
@@ -1,29 +1,33 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 const data = [
   {
-    image: '/assets/home/testimonials/1.webp',
+    image: "/assets/home/testimonials/1.webp",
+    image1: "/assets/premiumdesert/2.png",
     comment:
       "Richy Life Club made renting a convertible in Dubai hassle-free. No outrageous deposits, just straightforward service. The Audi TT I got was top-notch, and their respectful treatment ensures they're my go-to for luxury car rentals now.",
-    name: '- Thrilled Guest',
+    name: "- Thrilled Guest",
   },
   {
-    image: '/assets/home/testimonials/2.webp',
+    image: "/assets/home/testimonials/4.jpeg",
+    image1: "/assets/premiumdesert/1.png",
     comment:
-      'Jetskiing and water sports were a blast, thanks to their seamless arrangements. No hassle, just waves of fun.',
-    name: '- Joyful Adventurer',
+      "Jetskiing and water sports were a blast, thanks to their seamless arrangements. No hassle, just waves of fun.",
+    name: "- Joyful Adventurer",
   },
   {
-    image: '/assets/home/testimonials/3.webp',
+    image: "/assets/home/testimonials/3.webp",
+    image1: "/assets/membership/image2.avif",
     comment:
-      'No hidden fees or complications, just smooth sailing. The yacht was impeccable, and their friendly service made it an unforgettable experience',
-    name: '- Delighted Client',
+      "No hidden fees or complications, just smooth sailing. The yacht was impeccable, and their friendly service made it an unforgettable experience",
+    name: "- Delighted Client",
   },
   {
-    image: '/assets/home/testimonials/4.jpeg',
+    image: "/assets/home/testimonials/2.webp",
+    image1: "/assets/membership/image1.webp",
     comment:
       "No fuss, no hefty deposits—just a reliable car ready to roll. Excellent service that's now my go-to for stress-free car rentals",
-    name: '- Happy Customer',
+    name: "- Happy Customer",
   },
 ];
 
@@ -34,7 +38,7 @@ const Testimonials = () => {
         <div className="w-full grid grid-cols-1 sm:grid-cols-2" key={index}>
           <div
             className={`w-full relative h-full min-h-[279px] ${
-              index > 1 && 'order-1 sm:order-2'
+              index > 1 && "order-1 sm:order-2"
             }`}
             key={index}
           >
@@ -43,13 +47,30 @@ const Testimonials = () => {
               alt={item.name}
               fill
               style={{
-                objectFit: 'cover',
-                objectPosition: '10%',
+                objectFit: "cover",
+                objectPosition: "10%",
               }}
               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             />
           </div>
-
+          <div
+            className={`w-full relative h-full min-h-[279px] ${
+              index > 1 && "order-1 sm:order-2"
+            }`}
+            key={index}
+          >
+            <Image
+              src={item.image1}
+              alt={item.name}
+              fill
+              style={{
+                objectFit: "cover",
+                objectPosition: "10%",
+              }}
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            />
+          </div>
+          {/* 
           <div
             className={`${
               index % 2 !== 0
@@ -62,7 +83,7 @@ const Testimonials = () => {
           >
             <p className="text-sm">{item.comment}</p>
             <span className="font-medium">{item.name}</span>
-          </div>
+          </div> */}
         </div>
       ))}
     </div>
