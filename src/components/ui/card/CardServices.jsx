3e784ffc4dd8c8fcc71490e8358ae7883fe9327e
vishnu_p <vishnu.p@richylife.ae>
2024-07-15 15:02:59 +0400

    service card size and padding update and mobile resposive

--- src/components/ui/card/CardServices.jsx
+++ src/components/ui/card/CardServices.jsx
@@ -7,55 +7,11 @@ function CardServices({ service }) {
   return (
     <div>
       <div className="max-w-[1200px] mx-auto px-6 md:px-0">
-        {/* <div className="pb-10 grid grid-cols-1 md:grid-cols-2 mt-8 gap-7">
-          {data.map((e, index) => (
-            <Link href={`/`} key={index}>
-              <div
-                className={`min-h-[470px] flex flex-col ${
-                  index % 2 === 1 ? "md:mt-20" : ""
-                }`}
-              >
-                <Image
-                  src={e.image}
-                  width={400}
-                  height={300}
-                  alt="Title tags"
-                  className="rounded-none md:rounded"
-                  style={{
-                    objectFit: "cover",
-                    objectPosition: "center",
-                    width: "100%",
-                    height: "auto",
-                    aspectRatio: "calc(400/300)",
-                  }}
-                />
-                <div className="py-4 font-sans flex-auto flex justify-between flex-col">
-                  <div className="overflow-hidden">
-                    <h4 className="text-xl md:text-xl font-bold uppercase">
-                      {e.title}
-                    </h4>
-                    <p className="text-gray-500">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    </p>
-                  </div>
-                  <AnimatedBtn
-                    styles="w-fit text-black py-2 bg-opacity-80 rounded-sm border border-solid border-gray-500 border-opacity-50 hover:bg-[#CBB87E] hover:border-white"
-                    text={"Explore More"}
-                    msg={"Hi, I would like to know about your services."}
-                  />
-                </div>
-              </div>
-            </Link>
-          ))}
-        </div> */}
-
-        {/* ss */}
-
-        <div class="mt-8 pb-10 grid grid-cols-2 md:grid-cols-2 gap-4">
-          <div class="grid gap-4">
+        <div class="mt-8 pb-10 grid grid-cols-2 md:grid-cols-2 gap-16">
+          <div class="grid gap-4 pl-0 md:pl-16">
             {service.slice(0, Math.ceil(service.length / 2)).map((e, index) => (
               <Link href={e.attributes.url} key={index}>
-                <div className={`md:min-h-[470px] flex flex-col `}>
+                <div className={`md:min-h-[270px] flex flex-col `}>
                   <Image
                     src={e.attributes.image.data.attributes.url}
                     width={400}
@@ -91,13 +47,13 @@ function CardServices({ service }) {
               </Link>
             ))}
           </div>
-          <div class="grid gap-4 items-center pt-40">
+          <div class="grid gap-4 items-center pt-40 pr-0 md:pl-16">
             {service
               .slice(Math.ceil(service.length / 2), service.length)
               .map((e, index) => (
                 <Link href={e.attributes.url} key={index}>
                   <div
-                    className={`max-h-[470px] flex flex-col ${
+                    className={`max-h-[270px] flex flex-col ${
                       index === 0 ? "" : ""
                     } `}
                   >
@@ -136,7 +92,7 @@ function CardServices({ service }) {
                   </div>
                 </Link>
               ))}
-            <div className="min-h-[280px] flex flex-col"></div>
+            <div className="min-h-[150px] flex flex-col"></div>
           </div>
         </div>
       </div>
