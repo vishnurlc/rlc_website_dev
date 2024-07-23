cd452822fcbebe552547906fb630c325a763dedd
vishnu_p <vishnu.p@richylife.ae>
2024-07-23 18:09:12 +0400

    service page hero section text chnge

--- src/app/[lang]/service/page.jsx
+++ src/app/[lang]/service/page.jsx
@@ -38,6 +38,33 @@ export default async function page({ params, searchParams }) {
   // );
   // console.log("working", serviceData);
 
+  // const DestinationData = [
+  //   {
+  //     name: "UAE",
+  //     slug: "uae",
+  //     desc: "The UAE captivates with its blend of modern marvels and timeless traditions, showcasing dazzling skyscrapers alongside serene desert landscapes. Discover a world of luxury and innovation where ancient heritage meets cutting-edge sophistication.",
+  //     heading1: "Luxury Redefined,",
+  //     heading2: "Elegance Perfected",
+  //   },
+  //   {
+  //     name: "TURKIYE",
+  //     slug: "turkiye",
+  //     desc: "Turkey offers a mesmerizing blend of rich history and stunning landscapes, from ancient ruins to breathtaking coastlines. Experience a cultural tapestry woven with vibrant traditions and unparalleled natural beauty in this enchanting destination.",
+  //     heading1: "Where Timeless Wonders",
+  //     heading2: "Meet Modern Splendor",
+  //   },
+  //   {
+  //     name: "MALDIVES",
+  //     slug: "maldives",
+  //     desc: "The Maldives enchants with its pristine turquoise waters and overwater bungalows, creating a paradise of unparalleled beauty. Bask in the serene seclusion of its islands, where each moment is a perfect blend of luxury and natural splendor.",
+  //     heading1: "Pure Luxury, ",
+  //     heading2: "Infinite Serenity",
+  //   },
+  // ];
+  // const result = DestinationData.find(
+  //   (e) => e.slug === searchParams.destination
+  // );
+
   return (
     <div>
       <main className="overflow-hidden bg-[#F8F8F8]">
@@ -46,8 +73,8 @@ export default async function page({ params, searchParams }) {
         </h1>
         <Herosectionbyz
           type={"image"}
-          heading1={"ELEVATE"}
-          heading2={"YOUR ANY DAY..."}
+          heading1={"Pure Luxury,"}
+          heading2={"Infinite Serenity"}
           subheading={"Experience the sea breeze in luxury"}
           posterurl={"/assets/home/background.png"}
           btntext={"Book your trip"}
